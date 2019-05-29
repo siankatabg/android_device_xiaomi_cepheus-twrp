cd0b734fee9f266c78eb849ab23caa6ccce9ab8e
siankatabg <siankata91@gmail.com>
2019-05-29 08:42:01 +0100

    BoardConfig: Remove a duplicate line

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,6 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Workaround for error copying vendor files to recovery ramdisk
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 TARGET_COPY_OUT_VENDOR := vendor
 
 # Recovery
